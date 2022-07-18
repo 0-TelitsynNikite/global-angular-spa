04a9eff14497828bb222d7e76bf1df9454725221
Телицын Никита <dmtvn.labs@yandex.ru>
2022-07-18 18:45:42 +0300

    Added boxes-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'b2b', loadChildren: () => import('./domains/b2b/b2b.module').then(md => md.B2bModule)},
   {path: 'b2c', loadChildren: () => import('./domains/b2c/b2c.module').then(md => md.B2cModule)},
-  {path: 'design', loadChildren: () => import('./domains/design/design.module').then(md => md.DesignModule)}
+  {path: 'design', loadChildren: () => import('./domains/design/design.module').then(md => md.DesignModule)},
+  {path: 'boxes', loadChildren: () => import('./domains/boxes/boxes.module').then(md => md.BoxesModule)}
 ];
 
 @NgModule({
---
+++ src/app/domains/boxes/boxes.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  templateUrl: 'boxes.component.html',
+  styleUrls: ['boxes.component.scss'],
+  providers: []
+})
+export class BoxesComponent {}
